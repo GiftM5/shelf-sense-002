12ded4630953ad1f8a3ce935ddcc88f35f1b876f
GiftM5 <mofokeng_mpho@icloud.com>
2024-11-25 12:25:16 +0200

    Only added the write in function

--- functions/library_system.py
+++ functions/library_system.py
@@ -5,7 +5,9 @@ def initialize_database():
     Initialize the database file if it doesn't exist.
     """
     with open(DATABASE_FILE, 'a') as db:
+        Database = db
         pass  # Ensure the file exists
+
 
 def add_book(title, author):
     """
@@ -14,6 +16,15 @@ def add_book(title, author):
     :param author: The author of the book
     """
     # TODO: Append the book's title and author to the database file
+    with open(DATABASE_FILE,"w") as Database:
+        Database.write("")
+
+    Database = Database
+    print(Database) 
+
+        
+add_book("To Kill a Mockingbird","Harper Lee")
+
 
 def search_book(title):
     """
@@ -23,9 +34,23 @@ def search_book(title):
     """
     # TODO: Implement logic to search for a book in the database file
 
+    with open(DATABASE_FILE,"r") as db:
+        database = db.readlines
+
+    database = str(database)
+        
+    lines = database.split(",")
+    print(lines)
+
+
+search_book("To Kill a Mockingbird")
+
+
+
 def list_books():
     """
     List all books in the library.
     :return: A list of dictionaries with each book's details
     """
     # TODO: Read all books from the database file and return them as a list of dictionaries
+
